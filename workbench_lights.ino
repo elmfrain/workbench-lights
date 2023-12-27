a552f0940c303f56811bbcf9b0c73f42d33e9782
elmfrain <elmfer10@gmail.com>
2023-12-27 13:24:00 -0600

    revert to original input voltage constraints

--- workbench_lights.ino
+++ workbench_lights.ino
@@ -35,7 +35,7 @@ void loop()
 {
     float sysVoltage = sysStatus.getVoltage();
 
-    if(sysVoltage < 11 || 20 < sysVoltage)
+    if(sysVoltage < 17 || 20 < sysVoltage)
     {
         mainLED.setEnabled(false);
         auxLED.setEnabled(false);
